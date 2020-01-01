d7c82c4fbbf08ec53905409ac74c129896d27b0e
mugiwaradz <equilibriom6A>
2020-01-01 12:36:03 +0100

    using lambook && adding configurations

--- src/main/java/com/djallil/ems/model/Employee.java
+++ src/main/java/com/djallil/ems/model/Employee.java
@@ -1,9 +1,16 @@
 package com.djallil.ems.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "employees")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Setter
+@Getter
 public class Employee {
 
     @Column(name = "id")
@@ -26,69 +33,4 @@ public class Employee {
     @ManyToOne
     @JoinColumn(name = "reports_To", referencedColumnName = "id")
     private Employee reportsTo;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int employeeNumber) {
-        this.id = employeeNumber;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getExtension() {
-        return extension;
-    }
-
-    public void setExtension(String extension) {
-        this.extension = extension;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-
-    public Office getOfficeCode() {
-        return officeCode;
-    }
-
-    public void setOfficeCode(Office officeCode) {
-        this.officeCode = officeCode;
-    }
-
-    public Employee getReportsTo() {
-        return reportsTo;
-    }
-
-    public void setReportsTo(Employee reportsTo) {
-        this.reportsTo = reportsTo;
-    }
 }
--- src/main/java/com/djallil/ems/model/Office.java
+++ src/main/java/com/djallil/ems/model/Office.java
@@ -1,9 +1,16 @@
 package com.djallil.ems.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "offices")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Setter
+@Getter
 public class Office {
 
     @Column(name = "id")
@@ -26,86 +33,5 @@ public class Office {
     private String postalCode;
     @Column(name = "territory")
     private String territory;
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int officeCode) {
-        this.id = officeCode;
-    }
-
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-
-    public String getAddressLine1() {
-        return addressLine1;
-    }
-
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
-
-
-    public String getAddressLine2() {
-        return addressLine2;
-    }
-
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
-
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-
-    public String getTerritory() {
-        return territory;
-    }
-
-    public void setTerritory(String territory) {
-        this.territory = territory;
-    }
 
 }
